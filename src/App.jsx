194cc743a2a09dfa45ff24770e9336a070fd9dc5
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2023-06-20 15:03:50 +0100

    Included App component

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import { AppointmentsDayView } from "./AppointmentsDayView";
+import { sampleAppointments } from "./sampleData";
+
+export default function App() {
+  return <AppointmentsDayView appointments={sampleAppointments} />;
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { AppointmentsDayView } from "./AppointmentsDayView";
-import { sampleAppointments } from "./sampleData";
+import App from "./App";
 
 const root = createRoot(document.getElementById("app"));
 
-root.render(<AppointmentsDayView appointments={sampleAppointments} />);
+root.render(<App />);
